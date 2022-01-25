f58153fd4d6580c582bb4acc45aac45df3a5d50b
suciamalia09 <suciamalya09@gmail.com>
2022-01-25 16:01:30 +0700

    upload modul 2

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,14 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Suci Amalia Putriyanto",
         "email" => "suciamalya09@gmail.com",
         "gambar" => "suci.jpg"
@@ -30,5 +33,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
